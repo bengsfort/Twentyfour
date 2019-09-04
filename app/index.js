cc0861bf026363bc6ef2e12d08c6ed3f1fd631b1
Matt Bengston <matt.bengston@epicgames.com>
2019-09-04 20:49:14 +0300

    Add stats and junk

--- app/index.js
+++ app/index.js
@@ -1,11 +1,22 @@
 import clock from "clock";
 import document from "document";
+import { battery } from "power";
+import { me as appbit } from "appbit";
+import { today } from "user-activity";
 
 // Update every
 clock.granularity = "seconds";
 
+// Time
 const hourHand = document.getElementById("HourHand");
 const minutesProgress = document.getElementById("MinutesProgress");
+
+// Stats
+const dateText = document.getElementById("NowDate");
+const batteryText = document.getElementById("Battery");
+const batteryIcon = document.getElementById("BatteryIcon");
+const stepsText = document.getElementById("Steps");
+const stepsIcon = document.getElementById("StepsIcon");
 
 function hoursToAngle(hours, minutes) {
   const hourAngle = (360 / 24) * hours;
@@ -19,14 +30,49 @@ function minutesToArc(minutes, seconds) {
   return mins + secs;
 }
 
-function updateClock() {
-  const now = new Date();
-  const hours = now.getHours();
-  const mins = now.getMinutes();
-  const secs = now.getSeconds();
+function updateDate(date) {
+  const day = date.getUTCDate();
+  const month = date.getUTCMonth() + 1; // This is 0-11
+  const year = date.getUTCFullYear();
+  dateText.text = day + "." + month + "." + year;
+}
 
+function updateSteps() {
+  if (appbit.permissions.granted("access_activity")) {
+    stepsText.text = today.adjusted.steps;
+  }
+}
+
+function updateClock({ date }) {
+  const hours = date.getHours();
+  const mins = date.getMinutes();
+  const secs = date.getSeconds();
+
+  updateSteps();
+  updateDate(date);
   hourHand.groupTransform.rotate.angle = hoursToAngle(hours, mins);
   minutesProgress.sweepAngle = minutesToArc(mins, secs);
 }
 
-clock.ontick = () => updateClock();
+function updateBattery() {
+  batteryText.text = Math.floor(battery.chargeLevel) + "%";
+  if (battery.chargeLevel <= 20) {
+    batteryIcon.style.fill = "fb-red";
+  } else if (battery.chargeLevel <= 50) {
+    batteryIcon.style.fill = "fb-peach";
+  } else {
+    batteryIcon.style.fill = "fb-green";
+  }
+}
+
+if (appbit.permissions.granted("access_activity")) {
+  stepsIcon.style.visibility = "visible";
+  stepsText.style.visibility = "visible";
+} else {
+  stepsIcon.style.visibility = "hidden";
+  stepsText.style.visibility = "hidden";
+}
+
+updateBattery();
+clock.ontick = evt => updateClock(evt);
+battery.onchange = () => updateBattery();
